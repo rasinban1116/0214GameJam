3bb94d9e83cfadf4be5f7c90a1625a96d77eb936
arakyo <arakyo0411@gmail.com>
2020-02-14 15:38:18 +0900

    爆弾関係の途中

---
+++ 0214Game/Assets/arai/BombBulletSprict.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombBulletSprict : MonoBehaviour
+{
+
+    private int Way; // 弾の向き
+    private float TimeCount = 2; // 時間
+    public float speed;　// 弾の速度
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Way = Random.Range(0, 180);
+        this.transform.Rotate(0, 0, Way);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // タイマー
+        TimeCount -= Time.deltaTime;
+
+        // 向きに沿って進む
+        Vector3 Velocity = gameObject.transform.rotation * new Vector3(speed, 0, 0);
+        gameObject.transform.position += Velocity * Time.deltaTime;
+
+        // 2秒後に破壊される
+        if(TimeCount  < 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // プレイヤーに衝突したらプレイヤーにダメージ＆このオブジェクトを消滅させる
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+
+        }
+        // 爆弾に消滅したら爆弾にダメージ＆このオブジェクトを消滅させる
+        if (collision.gameObject.tag == "Bomb")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+
+}
---
+++ 0214Game/Assets/arai/TestPlayerBulletMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerBulletMove : MonoBehaviour
+{
+    private float Movex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Transform mytransform = this.transform;
+        Vector3 Pos = transform.position;
+        Pos.x += 0.01f;
+
+        mytransform.position = Pos;
+
+    }
+}
