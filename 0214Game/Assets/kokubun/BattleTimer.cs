fc89d2d9c2613d8d5d9365963ec011b012dfc8d7
kokubun_hiroki <kuronekobeer@gmail.com>
2020-02-14 16:01:22 +0900

    シーン遷移の追加、カウントダウンの追加

---
+++ 0214Game/Assets/kokubun/BattleTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleTimer : MonoBehaviour
+{
+    public Text timerText;
+
+    //合計時間
+    public float totalTime;
+    //表示用の時間データ
+    int second;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (totalTime >= 0)
+        {
+            totalTime -= Time.deltaTime;
+            second = (int)totalTime;
+            timerText.text = second.ToString();
+        }
+        if (totalTime < 0)
+        {
+            timerText.text = "スタート".ToString();
+        }
+    }
+}
