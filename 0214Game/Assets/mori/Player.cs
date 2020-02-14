e50281b87ad59a9665e345cce9058a1f48f9de84
kain0914 <kain120914@gmail.com>
2020-02-14 14:31:31 +0900

    プレイヤーのスクリプトの追加

---
+++ 0214Game/Assets/mori/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private const float WorkSpeed = 0.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            //右
+            transform.Translate(WorkSpeed, 0, 0);
+            //向き変更
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if(Input.GetAxis("Horizontal") < 0 )
+        {
+            //左
+            transform.Translate(-WorkSpeed, 0, 0);
+            //向き変更
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            //上
+            transform.Translate(0, WorkSpeed, 0);
+        }
+
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            //下
+            transform.Translate(0, -WorkSpeed, 0);
+        }
+    }
+}
